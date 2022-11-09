30f43c8625d9065ef9f0f15d5aa346ef7d0f0c45
Barlebo <mads_b_b@hotmail.com>
2022-11-09 11:42:54 +0100

    Ny

--- javascript/main.js
+++ javascript/main.js
@@ -29,6 +29,11 @@ dropdowns.forEach(dropdown => {
 });
 
 
+
+
+
+// Virker ikke
+
 // filterObjects("all")
 
 // function filterObjects(c){
@@ -62,33 +67,4 @@ dropdowns.forEach(dropdown => {
 //         }
 //     }
 //     element.className = arr1.join(" ");
-// }
-
-
-
-
-// let sortBtn = document.querySelector('.filter-menu').children;
-// let sortItem = document.querySelector('.filter-item').children;
-
-// for(let i = 0; i < sortBtn.length; i++){
-//     sortBtn[i].addEventListener('click', function(){
-//         for(let j = 0; j< sortBtn.length; j++){
-//             sortBtn[j].classList.remove('current');
-//         }
-
-//         this.classList.add('current');
-        
-
-//         let targetData = this.getAttribute('data-target');
-
-//         for(let k = 0; k < sortItem.length; k++){
-//             sortItem[k].classList.remove('active');
-//             sortItem[k].classList.add('delete');
-
-//             if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
-//                 sortItem[k].classList.remove('delete');
-//                 sortItem[k].classList.add('active');
-//             }
-//         }
-//     });
 // }
